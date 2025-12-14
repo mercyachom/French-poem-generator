6961270446381855e3b353dd92d04a688f278663
mercyachom <achommercy137@gmail.com>
2025-12-14 14:26:51 +0300

    added blink feature to my css and finished project

--- src/index.js
+++ src/index.js
@@ -15,13 +15,17 @@ function generatePoem(event) {
   let apiKey = "4bcdoaaff781057betcf5af877a53545";
   let prompt = `User Instructions: Generate a French Poem about ${instructionsInput.value}`;
   let context =
-    "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML. Sign the poem off with a bold'Shecodes AI' that is in the color of salmon. Please do not bold the work.  Make sure to follow the user instructions. I request that the text of html and single quotes at the beginning and quotes at the end of the poem are not included in the poem, please. Thank you";
+    "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 7 line poem in basic HTML. Please let the lines get spaced well. Sign the poem off with a bold'Shecodes AI' that is in the color of salmon. Please do not bold the work.  Make sure to follow the user instructions. I request that the text of html and single quotes at the beginning and quotes at the end of the poem are not included in the poem, please. Thank you";
 
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class = "generating">⏳Generating a French Poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
